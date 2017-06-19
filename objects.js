c9dd618529010c091d920a76ea223f569c27aea1
Michail <futurer@inbox.ru>
2017-06-19 23:55:28 +0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
-var obj = new Object();
-obj.param1 = "значение свойства param1";
-obj.param2 = { val1: "значение1", val2: 2};
+var obj = new Object();                       //создали объект
+obj.param1 = "значение свойства param1";      //добавили свойство со значением
+obj.param2 = { val1: "значение1", val2: 2};   //добавили ещё одно свойство-объект с 2мя свойствами внутри
 
 alert(obj.param1);
 alert(obj.param2.val1);
