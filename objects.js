66e81113edbe5bdbbcda5dd57fd7feab4a88d713
Michail <futurer@inbox.ru>
2017-06-19 23:56:23 +0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -2,5 +2,8 @@ var obj = new Object();                       //создали объект
 obj.param1 = "значение свойства param1";      //добавили свойство со значением
 obj.param2 = { val1: "значение1", val2: 2};   //добавили ещё одно свойство-объект с 2мя свойствами внутри
 
+
 alert(obj.param1);
 alert(obj.param2.val1);
+
+delete obj.param2.val2;   //удалили свойство
